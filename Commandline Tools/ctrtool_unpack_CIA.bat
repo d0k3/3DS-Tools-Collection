66d7dbc9785af233660a89d66fd034f52a3434ef
d0k3 <tore.anon@gmail.com>
2016-08-05 15:56:06 +0200

    Updated ctrtool & makerom

--- Commandline Tools/ctrtool_unpack_CIA.bat
+++ Commandline Tools/ctrtool_unpack_CIA.bat
@@ -1,2 +1,2 @@
 md %1_unpacked
-ctrtool --certs=%1_unpacked/certs.bin --tik=%1_unpacked/ticket.bin --tmd=%1_unpacked/tmd.bin --contents=%1_unpacked/contents %1
+ctrtool --certs=%1_unpacked/certs.bin --tik=%1_unpacked/ticket.bin --tmd=%1_unpacked/tmd.bin --meta=%1_unpacked/meta.bin --contents=%1_unpacked/contents %1
